7bd642bf517f3ae9bc2ad2b20215d8ddb3a52ff7
LeoDev2007 <leonardodeveloper200@gmail.com>
2024-11-12 22:03:48 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [image, setImage] = useState(null);
+
+  const obterPermissao = async () => {
+    const {granted} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!granted) {
+      alert('Permissão necessária');
+    }
+  };
+
+  const escolherImagem = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  React.useEffect(() => {
+    obterPermissao();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {image && (
+        <Image
+        source={{ uri: image }}
+        style={styles.image}
+        />
+      )}
+
+      <TouchableOpacity onPress={escolherImagem}>
+        <Text>Selecionar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +53,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginBottom: 20,
   },
 });
